df7b9763befa96d59aed5a5fd99a7aa116c578b8
vitor candido <vitor.cr018@gmail.com>
2022-09-02 14:20:21 -0300

    adding information

--- contatos/models.py
+++ contatos/models.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nome
+
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
@@ -12,5 +15,9 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
 
 
